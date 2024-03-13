b366e4b42fba5787c8c341dc305eae4a467b69b5
Anburaj S <anburaj2203@gmail.com>
2024-03-13 12:37:54 +0530

    created shopping cart with action

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import Joke from "./components/Joke";
 import Message from "./components/Message";
 import PostForm from "./components/PostForm";
 import Posts from "./components/Posts";
+import ShoppingCart from "./components/ShoppingCart";
 import Theme from "./components/Theme";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App w-[50%] m-auto">
       {/* <Joke /> */}
       {/* <Posts /> */}
       {/* <Message/> */}
       {/* <Theme/> */}
-      <PostForm/>
+      {/* <PostForm/> */}
+      <ShoppingCart/>
     </div>
   );
 }
